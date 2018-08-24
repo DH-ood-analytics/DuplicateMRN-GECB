2c085222f97061cd059653aa5ec115dbd304468d
tylercare <tyler.vickers@dignityhealth.org>
2018-08-24 14:58:06 -0700

    changing to generalized format

--- Duplicate_MRN_Report.R
+++ Duplicate_MRN_Report.R
@@ -1,7 +1,10 @@
-script_dir <- "C:/Users/tvickers/Desktop/Projects/Duplicate_MRN_INQ"
-script_dir <- "C:/Users/tvickers/Documents/GitRepositories/DuplicateMRN-GECB"
+### GLOBAL VARIABLE ###
+username <- Sys.getenv("USERNAME")
+#######################
 
-setwd("C:/Users/tvickers/Documents/GitRepositories/Utility_Scripts")
+script_dir <- paste0("C:/Users/", username, "/Documents/GitRepositories/DuplicateMRN-GECB")
+
+setwd(paste0("C:/Users/", username, "/Documents/GitRepositories/Utility_Scripts"))
 source("General_fxns.R")
 cleverSource_DateTable(return_directory = script_dir)
 
@@ -9,7 +12,18 @@ require(readxl)
 require(tidyr)
 require(dplyr)
 
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/input/BIDS Feed/dat_20180411")
+#set up environment
+
+#read in data
+
+#do initial cleaning/transformation
+
+#do logic
+
+#stage output
+
+
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/input/BIDS Feed/dat_20180704"))
 
 ca_colnames <- c("lastname", "firstname", "birthdate", "mrn", "ssn", "registrationdate",
                  "registeredby", "updateddate", "updatedby", "deleteddate", "deactivateddate",
@@ -19,7 +33,7 @@ ca_coltypes <- c("text", "text", "numeric", "text", "text", "numeric",
                  "text", "numeric", "text", "numeric", "numeric",
                  "text", "text", "text")
 
-iq_regactivity_ca <- read_excel("InQuicker IDX Patients 20180411.xlsx", skip=3, col_names = ca_colnames, col_types = ca_coltypes)
+iq_regactivity_ca <- read_excel("InQuicker IDX Patients 20180704.xlsx", skip=3, col_names = ca_colnames, col_types = ca_coltypes)
 
 iq_regactivity_ca$birthdate <-  as.Date(iq_regactivity_ca$birthdate, origin = '1899-12-30')
 iq_regactivity_ca$registrationdate <-  as.Date(iq_regactivity_ca$registrationdate, origin = '1899-12-30')
@@ -45,15 +59,15 @@ iq_regactivity_ca$baseline_duplication <- ifelse(!is.na(iq_regactivity_ca$commen
 iq_regactivity_ca$base_dupe_numeric <- ifelse(iq_regactivity_ca$baseline_duplication=="duplicate",1,0)
 
 #1 - flatten the 'standard duplicate' file from GECB into a single table. In the previous process, this was called "INQ_CA_all_time_dupes"
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/input/BIDS Feed/dat_20180411")
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/input/BIDS Feed/dat_20180704"))
 
 #get the sheetlist from the active file
-shts <- excel_sheets("Duplicate IDX Patients - Standard_201804.xlsx")
+shts <- excel_sheets("Duplicate IDX Patients - Standard_201807.xlsx")
 
 #read through data on each sheet and put into a list to be bound together afterwards.
 datalist.d1 = list()
 for(i in 1:length(shts)) {
-  dat <- read_excel("Duplicate IDX Patients - Standard_201804.xlsx", skip = 3, col_names = c("lastname", "firstname", "birthdate", "sex", "mrn", "ssn", "zip", "phone",
+  dat <- read_excel("Duplicate IDX Patients - Standard_201807.xlsx", skip = 3, col_names = c("lastname", "firstname", "birthdate", "sex", "mrn", "ssn", "zip", "phone",
                                                                                              "registrationdate", "registeredby", "updateddate", "updatedby", 
                                                                                              "comment", "groups"), col_types = "text", sheet = shts[i])
   dat$sheetname <- shts[i]
@@ -101,7 +115,7 @@ iq_regactivity_ca$cumsum_baseline <- cumsum(iq_regactivity_ca$base_dupe_numeric)
 iq_regactivity_ca$cumsum_aggressive <- cumsum(iq_regactivity_ca$agg_dupe_numeric)
 
 #6 - let's see what happens when I remove obvious test records
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/input/auxiliary_files")
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/input/auxiliary_files"))
 names_list <- read.csv("grab_test.csv", stringsAsFactors = F)
 test_names <- names_list %>% filter(names_list$name_type=="test") %>% select(names)
 
@@ -109,7 +123,7 @@ iq_regactivity_ca$is_test <- ifelse(iq_regactivity_ca$fullname_reg %in% test_nam
 
 #table is finalized--produce a few summary statistics.
 
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/output/Report")
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/output/Report"))
 #protect write-out
 #write.csv(iq_regactivity_ca, "duplicate_mrn_report.csv", row.names=F)
 
@@ -157,8 +171,8 @@ IQ_duplicates <- IQ_duplicates[,1:15]
 #write.csv(IQ_duplicates, "IQ_duplicates.csv", row.names = F)
 
 #add AZ
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/input/AZ Feed")
-iq_regactivity_az <- read_excel("InQuicker_MRN_Report_March.xls", skip = 1, col_names = c("mrn", "birthdate", "gender", "lastname", "firstname", "group",
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/input/AZ Feed"))
+iq_regactivity_az <- read_excel("InQuicker_MRN_Report_June.xls", skip = 1, col_names = c("mrn", "birthdate", "gender", "lastname", "firstname", "group",
                                                                                             "email", "citystate", "address_one", "address_two", "phonenumber",
                                                                                             "zip", "createdon", "createdby", "mergecomment"))
 iq_regactivity_az$mergecomment <- tolower(iq_regactivity_az$mergecomment)
@@ -170,7 +184,7 @@ iq_regactivity_az$createdon <- as.Date(iq_regactivity_az$createdon)
 iq_regactivity_az <- merge(iq_regactivity_az, date_table, by.x = "createdon", by.y = "day")
 
 #write out raw data for az
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/output/Report")
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/output/Report"))
 #protect write-out
 #write.csv(iq_regactivity_az, "duplicate_mrn_report_az.csv", row.names=F)
 
@@ -183,15 +197,25 @@ dupes_by_month_az <- iq_regactivity_az %>% group_by(yyyy_mm_dd) %>% summarise(cn
 
 
 ##add Inquicker Volume data
-setwd("C:/Users/tvickers/Documents/GitRepositories/InQuicker/input_data")
-source("source_inq_local.R")
+setwd(paste0("C:/Users/", username, "/Documents/GitRepositories/InQuicker/input_data"))
+source("source_inq_local_sharedtest.R")
 
 iq_master <- merge(iq_master, subset(date_table, select = c(day, yyyy_mm_dd)), by.x="registration_date", by.y="day")
 iq_master_grped <- iq_master %>% group_by(yyyy_mm_dd) %>% summarise(cnt = n())
 iq_master_grped$yyyy_mm_dd <- as.Date(iq_master_grped$yyyy_mm_dd, format="%Y-%m-%d")
-iq_master_grped <- iq_master_grped %>% filter(yyyy_mm_dd > "2015-02-01" & yyyy_mm_dd <= "2018-03-01")
+iq_master_grped <- iq_master_grped %>% filter(yyyy_mm_dd > "2015-02-01" & yyyy_mm_dd <= "2018-07-31")
 
 #write out raw data for INQ volume
-setwd("C:/Users/tvickers/Desktop/github_local/DuplicateMRN-GECB/output/Report")
+setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/output/Report"))
 #write.csv(iq_master_grped, "iq_master_grped.csv", row.names=F)
 
+#####adhoc/troubleshoot#####
+#evaluate my records against some samples that Linh provided on 4/5
+#test_linh <- iq_regactivity_ca %>% filter(registrationdate >= "2018-03-07" & updated_or_created_target=="InQuicker" & baseline_duplication == "duplicate")
+#find_linh <- iq_regactivity_ca %>% filter(lastname %in% c("CLARK", "ARAIZA", "GUAN") & firstname %in% c("BIANCA", "RICHARD", "SUNNY"))
+
+#find_linh <- iq_regactivity_ca %>% filter((lastname=="CLARK" & firstname=="BIANCA") | (lastname=="ARAIZA" & firstname=="RICHARD") | (lastname=="GUAN" & firstname=="SUNNY"))
+
+#setwd(paste0("C:/Users/", username, "/Box Sync/github_local/DuplicateMRN-GECB/output/auxiliary_files"))
+#write.csv(test_linh, "dupes_from_20180307_20180411.csv", row.names=F)
+#write.csv(find_linh, "review_linh_hits.csv", row.names=F)
